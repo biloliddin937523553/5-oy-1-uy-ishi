0adfe9b839ef4cf3c2b65c7e4c8e9272a8e2171a
biloliddin937523553 <biloliddinlatipov0@gmail.com>
2024-03-25 14:41:18 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+let son = prompt(`Birdan 7 gacha son kiriting`)
+
+switch (son){
+    case '1':
+        alert('duyshanba');
+        break;
+    case '2':
+        alert('seshanba');
+        break;
+    case '3':
+        alert('chorshanba');
+        break;
+    case '4':
+        alert('payshanba');
+        break;
+    case '5':
+        alert('juma');
+        break;
+    case '6':
+        alert('shanba');
+        break;
+    case'7':
+        alert('yakshanba')
+        break;
+    default:
+        alert('bir haftada yetti kun bor');
+        break;
+}
